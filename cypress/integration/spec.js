513d60b7fdb2354b3d131207c55ae7511968287b
Micha Pietsch <michapietsch@mail.de>
2019-04-04 14:32:54 +0200

    Use relative paths for Sqlite database names; use a dedicated .env file with Cypress

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,15 @@
 describe("Form", () => {
+    before(() => {
+        cy.exec("cp .env .env.backed_up_by_cypress");
+        cy.exec("cp .env.cypress .env");
+    });
+
     it("Guest can not view the form", () => {
         cy.visit("/form");
         cy.url().should("include", "/login");
+    });
+
+    after(() => {
+        cy.exec("mv .env.backed_up_by_cypress .env");
     });
 });
