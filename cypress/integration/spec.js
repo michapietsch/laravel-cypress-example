cc68dc2e461898326639b20b20c874365e6cebc2
Micha Pietsch <michapietsch@mail.de>
2019-04-04 15:35:53 +0200

    Use seeder to log in a user within Cypress tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,11 +2,21 @@ describe("Form", () => {
     before(() => {
         cy.exec("cp .env .env.backed_up_by_cypress");
         cy.exec("cp .env.cypress .env");
+        cy.exec("php artisan migrate:fresh");
+        cy.exec("php artisan db:seed --class CypressSeeder");
     });
 
     it("Guest can not view the form", () => {
         cy.visit("/form");
         cy.url().should("include", "/login");
+    });
+
+    it("The seeded user can login", () => {
+        cy.visit("/login");
+        cy.get("#email").type("some_user@example.com");
+        cy.get("#password").type("somePassword");
+        cy.get("button[type=submit]").click();
+        cy.url().should("include", "/home");
     });
 
     after(() => {
